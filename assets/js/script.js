0610a528a822e5f7ef5b266f3e276974eb4fd1d4
Dean85e <mxdeano@gmail.com>
2022-10-26 18:14:04 +0000

    Format code & Add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,13 @@
  let popup = document.getElementById("popup-window");
  let rules = document.getElementById("rules");
  const emoji = ["✊", "🖐️", "✌️"];
- openRules();
+ openRules(); // Calling the open rules function on page load.
  let startGame = document.getElementById("start-game");
  startGame.addEventListener("click", closeRules, restartGame);
  // Wait for the Dom content to be loaded.
  // Add Event listeners to the buttons.
  document.addEventListener("DOMContentLoaded", function () {
      let buttons = document.getElementsByTagName("button");
-
-
      // Loop through the buttons and listen for click selection.
      for (let button of buttons) {
          if (button.className === "select") {
@@ -20,26 +18,17 @@
                  let playerChoice = this.getAttribute("data-choice");
                  let reset = document.getElementById("restart");
                  reset.addEventListener("click", restartGame);
-
-
                  runGame(playerChoice);
-
              });
          }
-
      }
-
  });
  // Running game.
  function runGame(playerChoice, compChoice) {
-
-
-
-
-
      compChoice = computerChoice();
-     checkWinner(playerChoice, compChoice);         // converting player & computer choice to emoji's.
-     if (playerChoice === "Rock") {             
+     checkWinner(playerChoice, compChoice);
+     // converting player & computer choice to emoji's.
+     if (playerChoice === "Rock") {
          playerChoice = emoji[0];
      }
      if (playerChoice === "Paper") {
@@ -59,24 +48,23 @@
      }
      document.getElementById("your-emoji").innerHTML = playerChoice;
      document.getElementById("computer-emoji").innerHTML = compChoice;
-
-     if (playerScore >= 10 || computerScore >= 10) {                // if player or computer reaches 10 open  popup to display results.
+     // if player or computer reaches 10 open  popup to display results.
+     if (playerScore >= 10 || computerScore >= 10) {
          openPopup();
      }
-
-                                                                              // Add event listener to button on end result.
+     // Add event listener to button on end result.
      let removePopup = document.getElementById("close-popup");
      removePopup.addEventListener("click", closePopup);
      removePopup.addEventListener("click", restartGame);
-                                                                                       
-     if (playerScore >= 10) {                                               // Displaying the scores at game end
+     // Displaying the scores at game end
+     if (playerScore >= 10) {
          document.getElementById("result").innerHTML = "Player Wins!";
          document.getElementById("endinfo").innerHTML = "Player:" + playerScore + " " + " Computer:" + computerScore;
      } else if (computerScore >= 10) {
          document.getElementById("result").innerHTML = "Computer Wins!";
          document.getElementById("endinfo").innerHTML = "Player:" + playerScore + " " + " Computer:" + computerScore;
-     }
-     if (playerScore == 10 && computerScore > 7) {                       // Deciding which message gets displayed after the game has finished.
+     } // Deciding which message gets displayed after the game has finished.
+     if (playerScore == 10 && computerScore > 7) {
          document.getElementById("message").innerHTML = "Close call but victory is yours!";
      } else if (computerScore == 10 && playerScore > 7) {
          document.getElementById("message").innerHTML = "Close game better luck next time!";
@@ -89,40 +77,28 @@
      } else if (playerScore >= 4 <= 7 && computerScore == 10) {
          document.getElementById("message").innerHTML = "Unlucky, Try again";
      }
-
-
- }
-
- function computerChoice() {                                 // Random selection for computer choice.
-
+ } // Random selection for computer choice.
+ function computerChoice() {
      let compChoice = Math.floor(Math.random() * 3);
-
-
      switch (compChoice) {
          case 0:
              return "Rock";
-
 
          case 1:
              return "Scissors";
 
          case 2:
              return "Paper";
-
-
      }
-
  }
-                                                                 // Determine if player has won or computer has won.
+ // Determine if player has won or computer has won.
  function checkWinner(playerChoice, computerChoice) {
-
      if (playerChoice === computerChoice) {
 
          document.getElementById("your-choice").innerHTML = "Draw!";
          document.getElementById("computer-choice").innerHTML = "Draw!";
          return;
      }
-
      switch (playerChoice) {
          case "Rock":
              if (computerChoice === "Paper") {
@@ -157,59 +133,45 @@
                  document.getElementById("computer-choice").innerHTML = null;
                  incrementPlayerScore();
              }
-
      }
-
  }
-                                                                 // Increment player score & computer Score .
+ // Increment player score & computer Score .                                                              
  function incrementPlayerScore() {
      ++playerScore;
      document.getElementById("player-score").innerHTML = "Player: " + playerScore;
-
-
  }
 
  function incrementComputerScore() {
      ++computerScore;
      document.getElementById("computer-score").innerHTML = "Computer: " + computerScore;
-
-
-
  }
-
- function restartGame() {                            // Restart the game
+ // Restart the game
+ function restartGame() {
      playerScore = 0;
      computerScore = 0;
-
      document.getElementById("player-score").innerHTML = "Player: 0";
      document.getElementById("computer-score").innerHTML = "Computer: 0";
-
      document.getElementById("your-choice").innerHTML = " ";
      document.getElementById("computer-choice").innerHTML = " ";
-
      document.getElementById("your-emoji").innerHTML = "Ready";
      document.getElementById("computer-emoji").innerHTML = "Ready";
-
-
  }
-
- function openRules() {                         // Rules of the game.
+ // Rules of the game.
+ function openRules() {
      rules.classList.add("rule-text-popup");
      restartGame();
  }
-
- function closeRules() {                        // Close rules of the game.
+ // Close rules of the game.
+ function closeRules() {
      rules.classList.remove("rule-text-popup");
      restartGame();
  }
-
-
- function openPopup(playerScore, computerScore) {    // End game message.
-
+ // End game message.
+ function openPopup(playerScore, computerScore) {
      popup.classList.add("open-popup");
  }
-
- function closePopup() {                          // Close end game message and open rules.
+ // Close end game message and open rules.
+ function closePopup() {
      popup.classList.remove("open-popup");
      openRules();
  }
